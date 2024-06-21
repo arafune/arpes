dde8aaf955b5b4a7f8f577dfdc55d33268d2ce1b
Marek Kopciuszynski <mkopciuszynski@gmail.com>
2024-06-21 11:53:07 +0200

    Change test to match new example file data.

--- tests/test_prodigy_xy.py
+++ tests/test_prodigy_xy.py
@@ -12,7 +12,7 @@ data_dir = Path(__file__).parent.parent / "src" / "arpes" / "example_data"
 @pytest.fixture()
 def sample_xy() -> ProdigyXY:
     """Fixture."""
-    with Path(data_dir / "BLGr_GK_example_xy_data.xy").open(mode="r") as xy_file:
+    with Path(data_dir / "example_xy_data_si_bulk.xy").open(mode="r") as xy_file:
         xy_data: list[str] = xy_file.readlines()
     return ProdigyXY(xy_data)
 
@@ -24,21 +24,18 @@ class TestXY:
         """Test for reading xy file."""
         assert sample_xy.axis_info["d1"][1] == "eV"
         assert sample_xy.axis_info["d2"][1] == "nonenergy"
-        assert sample_xy.axis_info["d3"][1] == "polar"
         assert isinstance(sample_xy.params["detector_voltage"], float)
         assert isinstance(sample_xy.params["values_curve"], int)
-        np.testing.assert_almost_equal(sample_xy.params["eff_workfunction"], 4.31)
+        np.testing.assert_almost_equal(sample_xy.params["eff_workfunction"], 4.3)
         np.testing.assert_almost_equal(sample_xy.params["excitation_energy"], 21.2182)
-        np.testing.assert_almost_equal(sample_xy.axis_info["d1"][0][5], 20.080305)
-        np.testing.assert_almost_equal(sample_xy.axis_info["d3"][0][0], -65.0)
+        np.testing.assert_almost_equal(sample_xy.axis_info["d1"][0][5], 16.798332279411763)
 
     def test_integrated_intensity(self, sample_xy: ProdigyXY) -> None:
         """Test for integrated_intensity property."""
-        np.testing.assert_almost_equal(sample_xy.integrated_intensity, 242431787.20654204)
+        np.testing.assert_almost_equal(sample_xy.integrated_intensity, 1131362.930806)
 
     def test_convert_to_data_array(self, sample_xy: ProdigyXY) -> None:
         """Test for convert to xr.DataArray."""
         sample_data_array = sample_xy.to_data_array()
-        assert sample_data_array.dims == ("eV", "nonenergy", "polar")
-        assert sample_data_array.shape == (105, 100, 201)
-        np.testing.assert_almost_equal(sample_data_array.coords["polar"][0], np.deg2rad(-65.0))
+        assert sample_data_array.dims == ("eV", "nonenergy")
+        assert sample_data_array.shape == (137, 82)
