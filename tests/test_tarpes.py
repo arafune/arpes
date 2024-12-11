0bfbb5a043075008e6d7f3e0681c54df757e95de
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2024-12-11 18:22:11 +0900

    ✅  Revise mock_tarpes a bit.

--- tests/test_tarpes.py
+++ tests/test_tarpes.py
@@ -2,12 +2,19 @@
 
 import numpy as np
 import xarray as xr
+from numpy.typing import NDArray
 from scipy.special import erf
 
 from arpes.analysis import tarpes
 
 
-def temporal_from_rate(t: float, g: float, sigma: float, k_ex: float, t0: float = 0) -> float:
+def temporal_from_rate(
+    t: float | NDArray[np.float64],
+    g: float,
+    sigma: float,
+    k_ex: float,
+    t0: float = 0,
+) -> float | NDArray[np.float64]:
     """Temporal profile.
 
     From a rate equation, which is used in (for example)
@@ -25,21 +32,30 @@ def temporal_from_rate(t: float, g: float, sigma: float, k_ex: float, t0: float 
 
 
 n_data = 150
+pixel = 20
 position = np.linspace(100, 103, n_data)
 delaytime = np.linspace(-100e-15, 2500e-15, n_data)
 rng = np.random.default_rng(42)
 noise = rng.normal(loc=0, scale=0.01, size=n_data)
 tempo_intensity = (
-    temporal_from_rate(t=delaytime, g=1, sigma=50e-15, k_ex=2e12, t0=0.2e-12) + noise + 0.02
+    temporal_from_rate(
+        t=delaytime,
+        g=1,
+        sigma=50e-15,
+        k_ex=2e12,
+        t0=0.2e-12,
+    )
+    + noise
+    + 0.02
 )
 
 mock_tarpes = [
     xr.DataArray(
-        data=np.arange(400).reshape(20, 20) * tempo_intensity[i],
+        data=rng.integers(100, size=pixel * pixel).reshape(pixel, pixel) * tempo_intensity[i],
         dims=["phi", "eV"],
         coords={
-            "phi": np.linspace(np.deg2rad(-10), np.deg2rad(10), 20),
-            "eV": np.linspace(5, 6, 20),
+            "phi": np.linspace(np.deg2rad(-10), np.deg2rad(10), pixel),
+            "eV": np.linspace(5, 6, pixel),
         },
         attrs={"position": position[i], "id": int(i + 1)},
     )
@@ -54,11 +70,11 @@ def test_find_t_for_max_intensity():
         delayline_dim="position",
         delayline_origin=100.31,
     )
-    assert tarpes.find_t_for_max_intensity(tarpes_dataarray) == 886.9670772986807
+    assert tarpes.find_t_for_max_intensity(tarpes_dataarray) == 1021.2881894590657
     tarpes_dataarray = tarpes.build_crosscorrelation(
         mock_tarpes,
         delayline_dim="position",
         delayline_origin=100.31,
         convert_position_to_time=False,
     )
-    assert tarpes.find_t_for_max_intensity(tarpes_dataarray) == 0.13295302013422372
+    assert tarpes.find_t_for_max_intensity(tarpes_dataarray) == 0.15308724832215148
