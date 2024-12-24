5a63a1720babcd33bd18fabb5ce32364807bd743
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2024-12-24 10:08:46 +0900

    Refactor provenance attribute handling in shift_by and corrected_coords functions
    - Updated the shift_by function to correctly handle the provenance attribute by ensuring the "shift_coords" key is properly updated and assigned back to the DataArray's attributes.
    - Refactored the corrected_coords function to ensure the "coords_correction" key in the provenance attribute is correctly updated and assigned back to the DataArray's attributes.
    - Ensured consistency in handling the provenance attribute across both functions.

--- src/arpes/correction/coords.py
+++ src/arpes/correction/coords.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+import warnings
 from logging import DEBUG, INFO, Formatter, StreamHandler, getLogger
 from typing import TYPE_CHECKING, get_args
 
@@ -12,6 +13,8 @@ from arpes._typing import (
 )
 
 if TYPE_CHECKING:
+    from collections.abc import Sequence
+
     from arpes.provenance import Provenance
 
 LOGLEVELS = (DEBUG, INFO)
@@ -49,15 +52,77 @@ def shift_by(
     provenance_: Provenance = shifted_data.attrs.get("provenance", {})
     provenance_shift_coords = provenance_.get("shift_coords", [])
     provenance_shift_coords.append((coord_name, shift_value))
-    shifted_data.attrs["provenance"]["shift_coords"] = provenance_shift_coords
+    provenance_["shift_coords"] = provenance_shift_coords
+    shifted_data.attrs["provenance"] = provenance_
     return shifted_data
 
 
 def corrected_coords(
-    data: xr.DataArray, correction_types: CoordsOffset | tuple[CoordsOffset, ...]
+    data: xr.DataArray,
+    correction_types: CoordsOffset | Sequence[CoordsOffset],
 ) -> xr.DataArray:
+    """Corrects the coordinates of the given data by applying necessary transformations.
+
+    Args:
+        data (xr.DataArray): The input ARPES data array with coordinates to be corrected.
+        correction_types (CoordsOffset | tuple[CoordsOffset]): Correction types to be applied to the
+            data.
+
+    Returns:
+        xr.DataArray: The data array with corrected coordinates.
+    """
     if isinstance(correction_types, str):
         correction_types = (correction_types,)
     assert isinstance(correction_types, tuple)
+
+    corrected_data = data.copy(deep=True)
+
     for correction_type in correction_types:
         assert correction_type in get_args(CoordsOffset)
+
+        if "_offset" in correction_type:
+            coord_name = correction_type.split("_offset")[0]
+
+            if coord_name not in corrected_data.coords:
+                warnings.warn(
+                    f"{coord_name} has not been set, while you correct "
+                    f"{coord_name} by {correction_type}.",
+                    stacklevel=2,
+                )
+                continue
+
+            corrected_data = shift_by(
+                corrected_data,
+                coord_name,
+                -corrected_data.attrs[correction_type],
+            )
+            if coord_name in corrected_data.attrs:
+                corrected_data.attrs[coord_name] -= corrected_data.attrs[correction_type]
+
+        # angle correction by beta or theta
+        elif correction_type == "beta":
+            if corrected_data.S.is_slit_vertical:
+                corrected_data = shift_by(corrected_data, "phi", corrected_data.attrs["beta"])
+            else:
+                corrected_data = shift_by(corrected_data, "psi", corrected_data.attrs["beta"])
+            corrected_data.coords["beta"] = 0
+            corrected_data.attrs["beta"] = 0
+
+        elif correction_type == "theta":
+            if corrected_data.S.is_slit_vertical:
+                # Shift the corrected_data by the value of "theta" attribute along the "psi" axis
+                corrected_data = shift_by(corrected_data, "psi", corrected_data.attrs["theta"])
+            else:
+                # Shift the corrected_data by the value of "theta" attribute along the "phi" axis
+                corrected_data = shift_by(corrected_data, "phi", corrected_data.attrs["theta"])
+            corrected_data.coords["theta"] = 0
+            corrected_data.attrs["theta"] = 0
+
+        # provenance
+        provenance_: Provenance = corrected_data.attrs.get("provenance", {})
+        provenance_corrected_cords: list[CoordsOffset] = provenance_.get("coords_correction", [])
+        provenance_corrected_cords.append(correction_type)
+        provenance_["coords_correction"] = provenance_corrected_cords
+        corrected_data.attrs["provenance"] = provenance_
+
+    return corrected_data
