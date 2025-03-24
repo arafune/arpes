a63f5eaae6f790bcfa6917bb942f772ff3f7f888
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2025-03-24 12:10:27 +0900

    🎨  Ruff fix

--- src/arpes/correction/fermi_edge.py
+++ src/arpes/correction/fermi_edge.py
@@ -10,12 +10,11 @@ import xarray as xr
 from lmfit.models import LinearModel, QuadraticModel
 from matplotlib.axes import Axes
 
-from arpes.analysis.general import fit_fermi_edge
+from arpes.analysis import fit_fermi_edge
 from arpes.constants import TWO_DIMENSION
 from arpes.correction.intensity_map import shift_by
 from arpes.fits import GStepBModel, broadcast_model
 from arpes.provenance import Provenance, provenance, update_provenance
-from arpes.analysis import fit_fermi_edge
 
 if TYPE_CHECKING:
     from collections.abc import Callable, Hashable, Iterable
