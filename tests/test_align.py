2f5f06e974ec1e7e0218ba61399971ece8626f16
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2025-04-01 18:11:10 +0900

    ✅  Update tests

--- tests/test_align.py
+++ tests/test_align.py
@@ -11,7 +11,7 @@ def test_align2d_by_self_correlation(dataarray_cut: xr.DataArray):
 
 
 def test_align2d_by_self_correlation_subpixel(dataarray_cut: xr.DataArray):
-    align2d_values = align2d(dataarray_cut, dataarray_cut, subpixel=False)
+    align2d_values = align2d(dataarray_cut, dataarray_cut, subpixel=True)
     assert align2d_values[0] < 0.0002
     assert align2d_values[1] < 0.0002
 
