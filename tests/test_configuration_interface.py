566057fac7cca3e23863b61a29420be86ea4f626
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2025-07-07 19:17:46 +0900

    ✅  Test for configuration.interface

---
+++ tests/test_configuration_interface.py
@@ -0,0 +1,41 @@
+# test_configuration_interface.
+# Copyright (c) 2025 R. Arafune, All Rights Reserved.
+#
+
+from pathlib import Path
+import arpes.configuration.interface as interface
+
+
+def test_get_workspace_path():
+    """Return the absolute path to the current workspace directory."""
+    assert interface.get_workspace_path() == Path(".")
+
+
+def test_get_workspace_name():
+    """Return the absolute path to the current workspace directory."""
+    assert interface.get_workspace_name() == ""
+
+
+def test_get_data_path():
+    """Return the path to the data directory under the workspace."""
+    assert interface.get_data_path() is None
+
+
+def test_get_dataset_path():
+    """Return the path to the data directory under the workspace."""
+    assert interface.get_dataset_path() is None
+
+
+def test_get_figure_path():
+    """Return the path to the data directory under the workspace."""
+    assert interface.get_dataset_path() is None
+
+
+def test_loggin_file():
+    """Return the path to the data directory under the workspace."""
+    assert interface.get_logging_file() is None
+
+
+def test_get_logging_started():
+    """Return the path to the data directory under the workspace."""
+    assert interface.get_logging_started() is False
