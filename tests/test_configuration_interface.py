86271ec2ccde449635741d9a3bc5272b064bab5b
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2025-07-10 08:07:11 +0900

    🎨  Update test_configuration_interface.py

--- tests/test_configuration_interface.py
+++ tests/test_configuration_interface.py
@@ -27,11 +27,6 @@ def test_get_dataset_path():
     assert interface.get_dataset_path() is None
 
 
-def test_get_figure_path():
-    """Return the path to the data directory under the workspace."""
-    assert interface.get_dataset_path() is None
-
-
 def test_loggin_file():
     """Return the path to the data directory under the workspace."""
     assert interface.get_logging_file() is None
@@ -40,3 +35,36 @@ def test_loggin_file():
 def test_get_logging_started():
     """Return the path to the data directory under the workspace."""
     assert interface.get_logging_started() is False
+
+
+def test_get_figure_path():
+    """Return the path to the figure path under the workspace."""
+    assert interface.get_figure_path() is None
+
+
+def test_get_configu_value():
+    """Return the path to the figure path under the workspace."""
+    work_space = interface.get_config_value("WORKSPACE")
+    assert isinstance(work_space, dict)
+
+
+def test_get_setting_value():
+    """Return the path to the figure path under the workspace."""
+    interactive = interface.get_settings_value("interactive")
+    assert isinstance(interactive, dict)
+
+
+def test_get_full_config():
+    """Return the full configuration dictionary."""
+    config = interface.get_full_config()
+    assert isinstance(config, dict)
+    assert "WORKSPACE" in config
+    assert isinstance(config["WORKSPACE"], dict)
+
+
+def test_get_full_setting():
+    """Return the full settings dictionary."""
+    settings = interface.get_full_settings()
+    assert isinstance(settings, dict)
+    assert "interactive" in settings
+    assert isinstance(settings["interactive"], dict)
